d68db1f080f22e1e61fbfac389532a3bf1dcd42c
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2023-09-03 07:26:00 -0300

    cors

--- app/auth/routers.py
+++ app/auth/routers.py
@@ -72,7 +72,7 @@ async def verify_client(token: str):
         )
 
 
-@router.post("/login")
+@router.post("/login", status_code=200)
 async def login(form_login: OAuth2PasswordRequestForm = Depends()):
     return form_login
     if form_login.client_id is None:
